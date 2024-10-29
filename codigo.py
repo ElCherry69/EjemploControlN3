2b68f2bca3551858b44556291e323d140d24d7d9
ElCherry69 <thumasterkill2300@gmail.com>
2024-10-29 16:54:35 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -5,7 +5,7 @@ import numpy as np
 st.sidebar.title('Mi primera barra lateral de Streamlit')
 st.sidebar.header('¡Hola, Barra Lateral!')
 st.sidebar.write('Esto es una barra lateral')
-st.sidebar.image('BrawlStarForever')
+st.sidebar.image('BrawlStarForever.png')
 if st.sidebar.button('Haz clic pero en la barra lateral'):
     st.sidebar.write('¡Has hecho clic en el boton de la barra lateral!')
 user_input = st.sidebar.text_input('Escribe algo en la barra:')
